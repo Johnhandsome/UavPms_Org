191da0e3f699d6e308ad032c8ae6bb883f4166b9
an343 <An3439201@gmail.com>
2026-06-21 15:07:09 +0700

    feat: add monitor module queries for inspection history and mission status overview

---
+++ UavPms.Application/Features/Monitor/Queries/GetMissionStatusOverview/GetMissionStatusQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UavPms.Core.Interfaces.Repositories;
+
+namespace UavPms.Application.Features.Monitor.Queries.GetMissionStatusOverview;
+
+public class GetMissionStatusQueryHandler : IRequestHandler<GetMissionStatusQuery, MissionStatusOverviewDto>
+{
+    private readonly IMonitorRepository _monitorRepository;
+
+    public GetMissionStatusQueryHandler(IMonitorRepository monitorRepository)
+    {
+        _monitorRepository = monitorRepository;
+    }
+
+    public async Task<MissionStatusOverviewDto> Handle(GetMissionStatusQuery request, CancellationToken cancellationToken)
+    {
+        var model = await _monitorRepository.GetMissionStatusOverviewAsync(cancellationToken);
+
+        return new MissionStatusOverviewDto(
+            model.Pending,
+            model.InProgress,
+            model.Completed);
+    }
+}
