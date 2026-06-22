8a3bb976f12c88dffb6b7c408fb71c37a2d8b2cf
An <An3439201@gmail.com>
2026-06-22 18:42:27 +0700

    feat: initialize unit test project and add BCryptPasswordHasher test skeleton

---
+++ UavPms.UnitTests/Services/BCryptPasswordHasherTests.cs
@@ -0,0 +1,15 @@
+using UavPms.Infrastructure.Services;
+using Xunit;
+using FluentAssertions;
+
+namespace UavPms.UnitTests.Services;
+
+public class BCryptPasswordHasherTests
+{
+    private readonly BCryptPasswordHasher _hasher;
+
+    public class BCryptPasswordHasherTests
+    {
+        _hasher = new BCryptPasswordHasher();
+    }
+}
