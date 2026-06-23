e28b71cb441dd1108c5648f062c3011b2743ae15
An <An3439201@gmail.com>
2026-06-23 18:12:25 +0700

    test: add VerifyOtpCommandHandler tests and extend BCryptPasswordHasher test coverage

--- UavPms.UnitTests/Services/BCryptPasswordHasherTests.cs
+++ UavPms.UnitTests/Services/BCryptPasswordHasherTests.cs
@@ -14,6 +14,44 @@ public class BCryptPasswordHasherTests
     }
 
     [Fact]
+    public void Hash_ShouldNotReturnPlainText()
+    {
+        var password = "SuperSecretPassword123!@#";
+        var hash = _hasher.Hash(password);
+        
+        hash.Should().NotBeNullOrEmpty();
+        hash.Should().NotBe(password);
+        hash.Should().StartWith("$2");
+    }
+
+    public void Hash_ShouldProduceDifferentHashes_ForSamePassword()
+    {
+        var password = "SuperSecretPassword123!@#";
+        var hash1 = _hasher.Hash(password);
+        var hash2 = _hasher.Hash(password);
+        
+        hash1.Should().NotBe(hash2);
+    }
+
+    [Fact]
+    public void Verify_ShouldReturnTrue_WhenPasswordMatches()
+    {
+        var password = "SuperSecretPassword123!@#";
+        var hash = _hasher.Hash(password);
+        var result = _hasher.Verify(hash, password);
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Verity_ShouldReturnFalse_WhenPasswordDoesNotMatch()
+    {
+        var password = "SuperSecretPassword123!@#";
+        var hash = _hasher.Hash(password);
+        var result = _hasher.Verify(hash, "wrongPassword123!@#");
+        result.Should().BeFalse();
+    }
+
+    [Fact]
     public void Hash_ShouldReturnHashedPassword_WhenGivenPlanPassword()
     {
         var password = "SuperSecretPassword123!@#";
